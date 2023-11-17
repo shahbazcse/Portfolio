546a3d1bd84f1369e3a8281b988c8231341a4c5b
shahbazcse <cseshahbaz@gmail.com>
2023-11-17 21:15:04 +0530

    Added Profiles and Icons

--- shahbazahmad/src/components/NavBar.jsx
+++ shahbazahmad/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdEmail } from "react-icons/md";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -7,21 +8,46 @@ import { SiHashnode } from "react-icons/si";
 function NavBar() {
   return (
     <div className="flex flex-wrap justify-center items-center gap-8 my-6">
-      <div className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer">
+      <a
+        href="mailto:cseshahbaz@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+        className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer"
+      >
+        <MdEmail className="h-5 w-5 text-gray-300 bg-transparent" />
+      </a>
+      <a
+        href="https://twitter.com/shahbaz_cse"
+        target="_blank"
+        rel="noreferrer"
+        className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer"
+      >
         <FaSquareXTwitter className="h-5 w-5 text-gray-300 bg-transparent" />
-      </div>
-      <div className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer">
-        <FaSquareXTwitter className="h-5 w-5 text-gray-300 bg-transparent" />
-      </div>
-      <div className=" hover:bg-zinc-800 p-1.5 rounded-full cursor-pointer">
+      </a>
+      <a
+        href="https://github.com/shahbazcse"
+        target="_blank"
+        rel="noreferrer"
+        className=" hover:bg-zinc-800 p-1.5 rounded-full cursor-pointer"
+      >
         <FaGithub className="h-5 w-5 text-gray-300 bg-transparent" />
-      </div>
-      <div className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer">
+      </a>
+      <a
+        href="https://www.linkedin.com/in/shahbazcse"
+        target="_blank"
+        rel="noreferrer"
+        className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer"
+      >
         <FaLinkedin className="h-5 w-5 text-gray-300 bg-transparent" />
-      </div>
-      <div className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer">
+      </a>
+      <a
+        href="https://shahbazahmad.hashnode.dev"
+        target="_blank"
+        rel="noreferrer"
+        className=" hover:bg-zinc-800 p-1.5 rounded-md cursor-pointer"
+      >
         <SiHashnode className="h-5 w-5 text-gray-300 bg-transparent" />
-      </div>
+      </a>
     </div>
   );
 }
