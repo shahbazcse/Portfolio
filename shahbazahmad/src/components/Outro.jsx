9a3306604359e0c23ba78cdcfcd31eb04b58be97
shahbazcse <cseshahbaz@gmail.com>
2023-11-17 21:26:05 +0530

    Added Icons

--- shahbazahmad/src/components/Outro.jsx
+++ shahbazahmad/src/components/Outro.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import profilepicture from "../assets/shahbaz.jpg";
+import { PiArrowUpRightBold } from "react-icons/pi";
 
 function Outro() {
   return (
@@ -19,12 +20,12 @@ function Outro() {
           href="mailto:cseshahbaz@gmail.com"
           target="_blank"
           rel="noreferrer"
-          className="flex items-center gap-3 hover:bg-zinc-900 p-1.5 pt-0.5 rounded-md cursor-pointer mr-auto"
+          className="flex items-center gap-1.5 hover:bg-zinc-900 p-1.5 pt-0.5 rounded-md cursor-pointer mr-auto"
         >
           <span className="text-blue-600 underline underline-offset-4 cursor-pointer bg-transparent">
             cseshahbaz@gmail.com
           </span>
-          <span className="bg-transparent"></span>
+          <PiArrowUpRightBold className="h-5 w-5 mt-1.5 text-white bg-transparent" />
         </a>
       </div>
     </div>
