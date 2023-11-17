b7905309b7addc748dc6ef84959196c06977f4db
shahbazcse <cseshahbaz@gmail.com>
2023-11-17 21:34:10 +0530

    Added Icons

--- shahbazahmad/src/components/Footer.jsx
+++ shahbazahmad/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { BsTerminalFill } from "react-icons/bs";
 
 function Footer() {
   return (
     <div className="flex justify-between items-center py-[2vh] my-3 border-t-[1px] border-[#4e4e4e70] text-[0.8rem]">
       <div>Shahbaz © 2023</div>
-      <div>{"</>"}</div>
+      <BsTerminalFill className="h-5 w-5 mt-1 text-white bg-transparent" />
     </div>
   );
 }
