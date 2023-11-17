ee1c7b182f491282136b3fad609e40fa8df9a19c
shahbazcse <cseshahbaz@gmail.com>
2023-11-17 13:14:46 +0530

    Changed App Logo & Name

--- shahbazahmad/src/components/Introduction.jsx
+++ shahbazahmad/src/components/Introduction.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../assets/logo.png"
 
 function Introduction() {
   return (
-    <div className="flex flex-col lg:w-[45%] gap-3 my-4 text-md">
-      <div>logo</div>
+    <div className="flex flex-col lg:w-[45%] gap-3.5 my-4 text-md">
+      <div>
+        <img src={logo} alt="logo" className="h-10 w-10" />
+      </div>
       <div>I'm <span className="text-white">Shahbaz</span>, a developer, designer, and problem solver.</div>
       <div>
         As a full-stack developer I've built and shipped 12+ products (with over 160K users), been
