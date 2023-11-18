a829f91235b4408e30d56b8306660dc440d5ad5d
shahbazcse <cseshahbaz@gmail.com>
2023-11-18 21:49:26 +0530

    Added Project Data and Refactored Code

--- shahbazahmad/src/components/Projects/ProjectCard.jsx
+++ shahbazahmad/src/components/Projects/ProjectCard.jsx
@@ -2,38 +2,48 @@ import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { PiArrowSquareOutBold } from "react-icons/pi";
 
-function ProjectCard() {
+function ProjectCard({ project }) {
+  const { title, description, img, vid, tech, source, live, tags } = project;
   return (
     <div className="flex flex-col gap-3 bg-transparent h-max xl:w-[34vh] md:w-[38vh] w-full my-8 text-[0.9rem]">
       <img
-        src="https://placehold.co/600x400"
+        src={img || "https://placehold.co/600x400"}
         alt=""
         className="object-cover rounded-xl mb-2"
       />
-      <p className="text-white text-[1.1rem] font-bold tracking-wide">Title</p>
-      <p>
-        Description: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Iste unde deleniti sequi blanditiis quos? Dignissimos, veritatis error?
-        Dolorem, a quaerat.
+      <p className="text-white text-[1.1rem] font-bold tracking-wide">
+        {title}
       </p>
-      <div className="flex items-center gap-1 text-[0.8rem] font-poppins md:flex-wrap md:overflow-none md:h-fit overflow-x-auto h-[5vh]">
-        <div className="px-3 bg-gray-800 text-white rounded-full">React</div>
-        <div className="px-3 bg-gray-800 text-white rounded-full">Tailwind</div>
-        <div className="px-3 bg-gray-800 text-white rounded-full">Redux</div>
-        <div className="px-3 bg-gray-800 text-white rounded-full">Node.js</div>
-        <div className="px-3 bg-gray-800 text-white rounded-full">Node.js</div>
-        <div className="px-3 bg-gray-800 text-white rounded-full">Node.js</div>
-        <div className="px-3 bg-gray-800 text-white rounded-full">Node.js</div>
+      <p className="max-h-[9vh] overflow-auto">{description}</p>
+      <div className="flex items-start gap-1 text-[0.8rem] font-poppins md:flex-wrap md:overflow-none md:h-[6.9vh] overflow-x-auto h-fit">
+        {tech.map((i, index) => (
+          <div
+            key={index}
+            className="px-3 my-0.5 bg-gray-800 text-white rounded-full"
+          >
+            {i}
+          </div>
+        ))}
       </div>
       <div className="flex items-center gap-4 font-raleway text-[0.8rem] sm:mt-2">
-        <div className="flex items-center justify-center gap-1.5 px-4 py-1 bg-zinc-900 hover:bg-zinc-800 rounded-lg text-white font-bold cursor-pointer">
+        <a
+          href={source}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center justify-center gap-1.5 px-4 py-1 bg-zinc-900 hover:bg-zinc-800 rounded-lg text-white font-bold cursor-pointer"
+        >
           <span className="bg-transparent">Source</span>
           <AiFillGithub className="bg-transparent h-4 w-4" />
-        </div>
-        <div className="flex items-center justify-center gap-1.5 px-3 py-1 bg-zinc-900 hover:bg-zinc-800 rounded-lg text-white font-bold cursor-pointer">
+        </a>
+        <a
+          href={live}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center justify-center gap-1.5 px-3 py-1 bg-zinc-900 hover:bg-zinc-800 rounded-lg text-white font-bold cursor-pointer"
+        >
           <span className="bg-transparent">Live</span>
           <PiArrowSquareOutBold className="bg-transparent h-4 w-4 mb-0.5" />
-        </div>
+        </a>
       </div>
     </div>
   );
--- shahbazahmad/src/components/Projects/Projects.jsx
+++ shahbazahmad/src/components/Projects/Projects.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import { projects } from "../../data/projectsData";
 
 function Projects() {
-  const temp = [1, 2, 3, 4, 5, 6];
   return (
     <div className="h-fit flex items-center justify-between xl:gap-0 md:gap-8 flex-wrap xl:pb-6 md:pb-14">
-      {temp.map((i) => (
-        <ProjectCard />
+      {projects.map((project, index) => (
+        <ProjectCard key={index} project={project} />
       ))}
     </div>
   );
