88022e46ec0e8144d272976d09bc77a253e33443
shahbazcse <cseshahbaz@gmail.com>
2023-11-27 00:42:24 +0530

    Added Gif

--- shahbazahmad/src/components/Blogs/Blogs.jsx
+++ shahbazahmad/src/components/Blogs/Blogs.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import staytuned from "../../assets/staytuned.gif";
 
 function Blogs() {
   return (
     <div className="min-h-[48vh] h-fit flex items-center justify-between xl:gap-0 md:gap-8 flex-wrap xl:pb-6 md:pb-14">
-      <h1 className="text-center mx-auto mt-4">Coming soon..</h1>
+      {/* <h1 className="text-center mx-auto mt-4">Coming soon..</h1> */}
+      <img className="mx-auto h-[20vh] w-[32vh]" src={staytuned} />
     </div>
   );
 }
