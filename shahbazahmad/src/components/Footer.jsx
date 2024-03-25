42907c8ed3b8da33352cf47e396c274a386fb8f3
shahbazcse <cseshahbaz@gmail.com>
2024-03-26 01:29:29 +0530

    Changed Year

--- shahbazahmad/src/components/Footer.jsx
+++ shahbazahmad/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { BsTerminalFill } from "react-icons/bs";
 function Footer() {
   return (
     <div className="flex justify-between items-center py-4 my-3 border-t-[1px] border-[#4e4e4e70] text-[0.8rem]">
-      <div>Shahbaz © 2023</div>
+      <div>Shahbaz © 2024</div>
       <BsTerminalFill className="h-5 w-5 text-white bg-transparent" />
     </div>
   );
